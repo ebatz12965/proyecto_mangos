1feff437ca01639c50396534c278c882252d414a
ebatz12965 <ebatzm@miumg.edu.gt>
2024-10-21 21:22:09 -0600

    se corrigió la ruta de pedidos en api.php

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/pedidos', [PedidoController::class, 'index'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->get('/pedidos', [PedidoController::class, 'index']);
+
 
 
 Route::get('/pruebas', [PruebaController::class, 'index']);
